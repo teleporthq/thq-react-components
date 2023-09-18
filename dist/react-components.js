e126d7b159fe71c1440ede602c509e6c11dd0873
Vlad Nicula <vladnicula@gmail.com>
2023-09-18 13:06:31 +0300

    CMS Mixed Type changed to use only one key for switching

--- dist/react-components.js
+++ dist/react-components.js
@@ -1,88 +1,81 @@
-import E from "dayjs";
-import a, { useRef as f, useEffect as T, useState as d } from "react";
-const M = ({ date: e, format: t }) => {
-  const r = E.unix(new Date(e).getTime() / 1e3), s = E(r).format(t);
-  return /* @__PURE__ */ a.createElement(a.Fragment, null, s);
-}, S = ({ html: e }) => {
-  const t = f(null);
-  return T(() => {
-    if (!e || !t.current)
+import p from "dayjs";
+import n, { useRef as f, useEffect as E, useState as d } from "react";
+const I = ({ date: t, format: e }) => {
+  const r = p.unix(new Date(t).getTime() / 1e3), a = p(r).format(e);
+  return /* @__PURE__ */ n.createElement(n.Fragment, null, a);
+}, M = ({ html: t }) => {
+  const e = f(null);
+  return E(() => {
+    if (!t || !e.current)
       return;
-    const r = document.createRange().createContextualFragment(e);
-    t.current.append(r);
-  }, []), /* @__PURE__ */ a.createElement("div", { style: { display: "contents" }, ref: t });
-}, x = (e) => {
+    const r = document.createRange().createContextualFragment(t);
+    e.current.append(r);
+  }, []), /* @__PURE__ */ n.createElement("div", { style: { display: "contents" }, ref: e });
+}, S = (t) => {
   const {
-    fetchData: t,
+    fetchData: e,
     params: r,
-    initialData: s,
-    persistDataDuringLoading: n = !1,
-    renderLoading: u = () => null,
-    renderSuccess: c,
-    renderError: l = () => null
-  } = e, [D, o] = d("idle"), [i, y] = d(s), [h, F] = d(null), p = f(
-    e.initialData !== void 0
-  ), g = f(n);
-  switch (g.current = n, T(() => {
-    if (p.current) {
-      p.current = !1;
+    initialData: a,
+    persistDataDuringLoading: c = !1,
+    renderLoading: l = () => null,
+    renderSuccess: s,
+    renderError: u = () => null
+  } = t, [R, i] = d("idle"), [o, g] = d(a), [F, h] = d(null), y = f(
+    t.initialData !== void 0
+  ), D = f(c);
+  switch (D.current = c, E(() => {
+    if (y.current) {
+      y.current = !1;
       return;
     }
     (async () => {
-      o("loading"), g.current || y(void 0);
+      i("loading"), D.current || g(void 0);
       try {
-        const m = await t(r);
-        y(m), o("success");
+        const m = await e(r);
+        g(m), i("success");
       } catch (m) {
-        F(m), o("error");
+        h(m), i("error");
       }
     })();
-  }, [r, t]), D) {
+  }, [r, e]), R) {
     case "idle":
     case "loading":
-      return e.persistDataDuringLoading && i ? c(i, !0) : u();
+      return t.persistDataDuringLoading && o ? s(o, !0) : l();
     case "success":
-      return c(i, !1);
+      return s(o, !1);
     case "error":
-      return l(h);
+      return u(F);
     default:
       return null;
   }
-}, B = (e) => {
-  const { items: t, renderItem: r, renderEmpty: s } = e;
-  if ("data" in t && "meta" in t) {
-    const { data: n, meta: u } = t;
-    return Array.isArray(n) === !1 ? null : n.length === 0 ? s ? s() : null : /* @__PURE__ */ a.createElement(a.Fragment, null, n.map(
-      (c, l) => r(typeof c == "object" ? { ...c, teleportMeta: u } : c, l)
+}, w = (t) => {
+  const { items: e, renderItem: r, renderEmpty: a } = t;
+  if ("data" in e && "meta" in e) {
+    const { data: c, meta: l } = e;
+    return Array.isArray(c) === !1 ? null : c.length === 0 ? a ? a() : null : /* @__PURE__ */ n.createElement(n.Fragment, null, c.map(
+      (s, u) => r(typeof s == "object" ? { ...s, teleportMeta: l } : s, u)
     ));
   }
-  return Array.isArray(t) ? t.length === 0 ? s ? s() : null : /* @__PURE__ */ a.createElement(a.Fragment, null, t.map((n, u) => r(n, u))) : null;
-}, R = ({ src: e, description: t }) => /* @__PURE__ */ a.createElement(a.Fragment, null, e && /* @__PURE__ */ a.createElement(
+  return Array.isArray(e) ? e.length === 0 ? a ? a() : null : /* @__PURE__ */ n.createElement(n.Fragment, null, e.map((c, l) => r(c, l))) : null;
+}, A = ({ src: t, description: e }) => /* @__PURE__ */ n.createElement(n.Fragment, null, t && /* @__PURE__ */ n.createElement(
   "img",
   {
     loading: "lazy",
-    src: e,
-    alt: t ?? ""
+    src: t,
+    alt: e ?? ""
   }
-)), H = ({ node: e, children: t }) => e.attrs.src ? /* @__PURE__ */ a.createElement(R, { ...e.attrs }) : /* @__PURE__ */ a.createElement(a.Fragment, null, t), b = (e, t) => t?.components?.[e.subType] ? [
-  !0,
-  t.components[e.subType]?.(e.attributes)
-] : [!1], A = (e, t) => t.contentTypes?.[e.subType] ? [
-  !0,
-  t.contentTypes[e.subType]?.(e.attributes)
-] : [!1], j = ({ itemData: e, mappingConfiguration: t, renderDefault: r, renderError: s }) => {
+)), x = ({ node: t, children: e }) => t.attrs.src ? /* @__PURE__ */ n.createElement(A, { ...t.attrs }) : /* @__PURE__ */ n.createElement(n.Fragment, null, e), H = ({ itemData: t, mappingConfiguration: e, renderDefault: r, renderError: a }) => {
   try {
-    const n = e.type === "component" ? b(e, t) : A(e, t);
-    return /* @__PURE__ */ React.createElement(React.Fragment, null, n[0] ? n[1] ?? null : r ? r(e) : "default case");
-  } catch (n) {
-    return /* @__PURE__ */ React.createElement(React.Fragment, null, s ? s(n) : "error case");
+    return e?.[t.typeId] ? /* @__PURE__ */ React.createElement(React.Fragment, null, e[t.typeId]?.(t.attributes) ?? null) : /* @__PURE__ */ React.createElement(React.Fragment, null, r ? r(t) : "default case");
+  } catch (c) {
+    return /* @__PURE__ */ React.createElement(React.Fragment, null, a ? a(c) : "error case");
   }
 };
 export {
-  j as CMSMixedType,
-  H as CaisyDocumentLink,
-  S as DangerousHTML,
-  x as DataProvider,
-  M as DateTimePrimitive,
-  B as Repeater
+  H as CMSMixedType,
+  x as CaisyDocumentLink,
+  M as DangerousHTML,
+  S as DataProvider,
+  I as DateTimePrimitive,
+  w as Repeater
 };
--- src/cms-mixed-type/index.tsx
+++ src/cms-mixed-type/index.tsx
@@ -1,60 +1,27 @@
-const switchBasedOnComponent = (
-    itemData: ItemDataType,
-    mappingConfiguration: MappingConfigurationType
-  ) => {
-    if (mappingConfiguration?.components?.[itemData.subType]) {
-      return [
-        true,
-        mappingConfiguration.components[itemData.subType]?.(itemData.attributes)
-      ];
-    }
-    return [false];
-  };
-  
-const switchBasedOnContentType = (
-    itemData: ItemDataType,
-    mappingConfiguration: MappingConfigurationType
-  ) => {
-    if (mappingConfiguration.contentTypes?.[itemData.subType]) {
-      return [
-        true,
-        mappingConfiguration.contentTypes[itemData.subType]?.(itemData.attributes)
-      ];
-    }
-    return [false];
-  };
-
-
 // These are permisive props, 
 // we wont enforce type safety on
 // props for each case
-type ItemDataType = Record<string, unknown> & { type: 'component' | 'content-type', subType: string}
-type MappingConfigurationType = {
-    components: {
-        [key: string]: (props: { [key: string]: any }) => JSX.Element;
-    };
-    contentTypes: {
-        [key: string]: (props: { [key: string]: any }) => JSX.Element;
-    };
+type ItemDataType = Record<string, unknown> & {typeId: string, attributes?: Record<string, unknown>};
+export type MappingConfigurationType = {
+  [key: string]: (props: { [key: string]: any }) => JSX.Element;
 }
 
 export interface CMSMixedTypeProps {
-    itemData: ItemDataType,
-    mappingConfiguration: MappingConfigurationType
-    renderDefault?: (itemData: ItemDataType) => JSX.Element
-    renderError?: (err: any) => JSX.Element
+  itemData: ItemDataType,
+  mappingConfiguration: MappingConfigurationType
+  renderDefault?: (itemData: ItemDataType) => JSX.Element
+  renderError?: (err: any) => JSX.Element
 }
 
 export const CMSMixedType = ({itemData, mappingConfiguration, renderDefault, renderError}: CMSMixedTypeProps) => {
-    try {
-        const result =
-            itemData.type === "component"
-            ? switchBasedOnComponent(itemData, mappingConfiguration)
-            : switchBasedOnContentType(itemData, mappingConfiguration);
-        return <>{result[0] ? result[1] ?? null : (renderDefault ? renderDefault(itemData) : "default case")}</>;
-      } catch (err) {
-        return <>{renderError ? renderError(err) : "error case"}</>
+  try {
+      if ( mappingConfiguration?.[itemData.typeId] ) {
+        return <>{mappingConfiguration[itemData.typeId]?.(itemData.attributes) ?? null}</>
       }
+      return <>{renderDefault ? renderDefault(itemData) : "default case"}</>
+    } catch (err) {
+      return <>{renderError ? renderError(err) : "error case"}</>
+    }
 };
 
 export default CMSMixedType
